bf653fe7c27b51e72e2442b1b644d87005701c73
MaximeAverty <maxime.averty.pro@gmail.com>
2024-10-26 17:18:24 +0200

    Initial commit

---
+++ src/main/java/me/bmo84/bMOsCasino/BMOsCasino.java
@@ -0,0 +1,47 @@
+package me.bmo84.bMOsCasino;
+
+import net.milkbowl.vault.economy.Economy;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.RegisteredServiceProvider;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BMOsCasino extends JavaPlugin {
+
+    private static Economy econ = null;
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        Bukkit.getServer().getLogger().info("Server demarré");
+
+        if (!setupEconomy() ) {
+            getLogger().severe(String.format("[%s] - Disabled due to no Vault dependency found!", getDescription().getName()));
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+
+    private boolean setupEconomy() {
+        if (getServer().getPluginManager().getPlugin("Vault") == null) {
+            return false;
+        }
+        RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
+        if (rsp == null) {
+            return false;
+        }
+        econ = rsp.getProvider();
+        return econ != null;
+    }
+
+    public static Economy getEconomy() {
+        if(econ != null) {
+            return econ;
+        }
+        return null;
+    }
+}
